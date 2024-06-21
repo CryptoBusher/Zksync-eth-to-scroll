e8438f73ed8b73132c6f0f93b9d7d9ac828e9ef4
CryptoBusher <garlicplan@gmail.com>
2024-06-21 12:49:04 +0300

    Config update

--- userConfig.js
+++ userConfig.js
@@ -1,8 +1,8 @@
 export const userConfig = {
-    accDelaySec: [120, 150],
+    accDelaySec: [1800, 5400],
     minimalBalanceEth: 0.0005,
     slippages: [0.1, 0.2],
-    maxFeeEth: 0.0003,
+    maxFeeEth: 0.00035,
     bridgesToUse: {
         'routerNitro': 1
     },
